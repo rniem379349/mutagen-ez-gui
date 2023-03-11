4b67f2d11dd57ba6fb1e081a1995db34b9ea1792
rniem <robert.niemiec95@wp.pl>
2023-03-11 13:24:28 +0100

    refactor, visual tweaks

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import mutagen
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.graphics import Color
 from kivy.properties import NumericProperty, ObjectProperty, ReferenceListProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -18,16 +19,11 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.vector import Vector
 from mutagen.id3 import ID3, ID3NoHeaderError
-from mutagen.mp3 import MP3
+from mutagen.mp3 import MP3, HeaderNotFoundError
 from mutagen.mp4 import MP4
-from mutagen.wave import WAVE
+from mutagen.wave import WAVE, _WaveID3
 
-from metadata_setters import (
-    ID3MetadataSetter,
-    MetadataDisplayPanel,
-    MP4MetadataSetter,
-    VorbisMetadataSetter,
-)
+from metadata_setters import ID3MetadataSetter, MP4MetadataSetter, VorbisMetadataSetter
 
 
 class MutagenMetadataInputGroup(BoxLayout):
@@ -35,10 +31,30 @@ class MutagenMetadataInputGroup(BoxLayout):
     Widget for setting a particular metadata tag.
     Contains a text input for the metadata, and a button to set the tag.
     Needs a metadata tag key, and a list of files to open and add metadata to.
+    Can be supplied a reference to the display panel to instantly
+    reflect metadata changes when setting tags.
     """
 
     metadata_key = ""
     selection = []
+
+    def __init__(self, metadata_key, **kwargs):
+        self.metadata_display_panel = kwargs.pop("metadata_display_panel", None)
+        super().__init__(**kwargs)
+        self.orientation = "horizontal"
+        self.metadata_key = metadata_key
+        self.metadata_input = TextInput()
+        self.apply_button = Button(
+            halign="center",
+            padding=(5, 5),
+            text="Set {}".format(metadata_key.capitalize().replace("_", " ")),
+            text_size=(120, None),
+            size_hint_x=None,
+            width=120,
+        )
+        self.apply_button.on_press = self.set_metadata
+        self.add_widget(self.metadata_input)
+        self.add_widget(self.apply_button)
 
     def set_metadata(self):
         """
@@ -47,19 +63,19 @@ class MutagenMetadataInputGroup(BoxLayout):
         """
         for filepath in self.selection:
             try:
-                tags = mutagen.File(filepath)
-                if isinstance(tags, WAVE):
-                    if tags.tags is None:
-                        tags.add_tags()
+                filething = mutagen.File(filepath)
+                if isinstance(filething, WAVE):
+                    if filething.tags is None:
+                        filething.add_tags()
                     metadata_setter = ID3MetadataSetter(
-                        tags, filepath, self.metadata_key
+                        filething, filepath, self.metadata_key
                     )
-                elif isinstance(tags, MP4):
+                elif isinstance(filething, MP4):
                     metadata_setter = MP4MetadataSetter(
-                        tags, filepath, self.metadata_key
+                        filething, filepath, self.metadata_key
                     )
                 elif isinstance(
-                    tags.tags,
+                    filething.tags,
                     (
                         mutagen.oggvorbis.OggVCommentDict,
                         mutagen.oggopus.OggOpusVComment,
@@ -67,33 +83,23 @@ class MutagenMetadataInputGroup(BoxLayout):
                     ),
                 ):
                     metadata_setter = VorbisMetadataSetter(
-                        tags, filepath, self.metadata_key
+                        filething, filepath, self.metadata_key
                     )
-                elif isinstance(tags, MP3) or tags.tags is None:
+                elif isinstance(filething, MP3) or filething.tags is None:
                     try:
-                        tags = ID3(filepath)
+                        filething = ID3(filepath)
                     except ID3NoHeaderError:
-                        tags = ID3()
+                        filething = ID3()
                     metadata_setter = ID3MetadataSetter(
-                        tags, filepath, self.metadata_key
+                        filething, filepath, self.metadata_key
                     )
             except Exception as exc:
                 print("Couldn't open file {}, exception: {}".format(filepath, exc))
                 continue
             metadata_setter.set_tag(self.metadata_input.text)
             metadata_setter.save_tags_to_file()
-
-    def __init__(self, metadata_key, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "horizontal"
-        self.metadata_key = metadata_key
-        self.metadata_input = TextInput()
-        self.apply_button = Button(
-            text="Set {}".format(metadata_name.capitalize().replace("_", " "))
-        )
-        self.apply_button.on_press = self.set_metadata
-        self.add_widget(self.metadata_input)
-        self.add_widget(self.apply_button)
+        if self.metadata_display_panel and len(self.selection) == 1:
+            self.metadata_display_panel.set_metadata_labels(self.selection[0])
 
 
 class FileExplorer(FileChooserIconView):
@@ -127,7 +133,11 @@ class MetadataDisplayRow(BoxLayout):
         self.key_label.halign = "left"
         self.key_label.valign = "middle"
         self.key_label.text = metadata_key.capitalize().replace("_", " ")
-        self.value_label = Label()
+        self.value_label = Label(
+            text_size=(180, None),
+            halign="center",
+            valign="middle",
+        )
         self.add_widget(self.key_label)
         self.add_widget(self.value_label)
 
@@ -140,6 +150,8 @@ class MetadataDisplayPanel(GridLayout):
         super().__init__(**kwargs)
         self.rows = 5
         self.cols = 1
+        self.padding = (0, 10)
+
         for key in self.metadata_keys:
             display_widget = MetadataDisplayRow(key)
             self.metadata_widgets[key] = display_widget
@@ -147,27 +159,34 @@ class MetadataDisplayPanel(GridLayout):
 
     def get_metadata(self, filepath):
         try:
-            tags = File(filepath, easy=True)
-            if isinstance(tags.tags, _WaveID3):
-                artist = ", ".join(getattr(tags.get("TPE1", None), "text", ["Unknown"]))
-                album = ", ".join(getattr(tags.get("TALB", None), "text", ["Unknown"]))
-                title = ", ".join(getattr(tags.get("TIT2", None), "text", ["Unknown"]))
+            filething = mutagen.File(filepath, easy=True)
+            # edge case: _WaveID3 doesn't seem to implement the mutagen easy tag functionality
+            if isinstance(filething.tags, _WaveID3):
+                artist = ", ".join(
+                    getattr(filething.get("TPE1", None), "text", ["Unknown"])
+                )
+                album = ", ".join(
+                    getattr(filething.get("TALB", None), "text", ["Unknown"])
+                )
+                title = ", ".join(
+                    getattr(filething.get("TIT2", None), "text", ["Unknown"])
+                )
                 date_stringified = [
                     str(timestamp)
                     for timestamp in getattr(
-                        tags.get("TDRC", None), "text", ["Unknown"]
+                        filething.get("TDRC", None), "text", ["Unknown"]
                     )
                 ]
                 date = ", ".join(date_stringified)
                 tracknumber = ", ".join(
-                    getattr(tags.get("TRCK", None), "text", ["Unknown"])
+                    getattr(filething.get("TRCK", None), "text", ["Unknown"])
                 )
             else:
-                artist = tags.get("artist", "Unknown")
-                album = tags.get("album", "Unknown")
-                title = tags.get("title", "Unknown")
-                date = tags.get("date", "Unknown")
-                tracknumber = tags.get("tracknumber", "Unknown")
+                artist = filething.get("artist", "Unknown")
+                album = filething.get("album", "Unknown")
+                title = filething.get("title", "Unknown")
+                date = filething.get("date", "Unknown")
+                tracknumber = filething.get("tracknumber", "Unknown")
         except (HeaderNotFoundError, AttributeError):
             artist = "Unknown"
             album = "Unknown"
@@ -184,7 +203,6 @@ class MetadataDisplayPanel(GridLayout):
 
     def set_metadata_labels(self, filepath):
         tags = self.get_metadata(filepath)
-        print("tags", tags)
         for key in self.metadata_keys:
             value = tags[key]
             if isinstance(value, list):
@@ -210,14 +228,42 @@ class MutaGUIMain(BoxLayout):
         super().__init__(**kwargs)
         self.orientation = "horizontal"
         # panel for setting and displaying metadata
-        self.metadata_panel = GridLayout(cols=1, rows=6, row_default_height=50)
+        self.metadata_panel = GridLayout(
+            cols=1,
+            rows=6,
+        )
         # input groups
-        self.artist_input_group = MutagenMetadataInputGroup("artist")
-        self.album_input_group = MutagenMetadataInputGroup("album")
-        self.title_input_group = MutagenMetadataInputGroup("title")
-        self.track_number_input_group = MutagenMetadataInputGroup("track_number")
-        self.recording_year_input_group = MutagenMetadataInputGroup("recording_year")
-        self.metadata_display_panel = MetadataDisplayPanel()
+        self.metadata_display_panel = MetadataDisplayPanel(size_hint_y=None, height=240)
+        self.artist_input_group = MutagenMetadataInputGroup(
+            "artist",
+            metadata_display_panel=self.metadata_display_panel,
+            size_hint_y=None,
+            height=50,
+        )
+        self.album_input_group = MutagenMetadataInputGroup(
+            "album",
+            metadata_display_panel=self.metadata_display_panel,
+            size_hint_y=None,
+            height=50,
+        )
+        self.title_input_group = MutagenMetadataInputGroup(
+            "title",
+            metadata_display_panel=self.metadata_display_panel,
+            size_hint_y=None,
+            height=50,
+        )
+        self.track_number_input_group = MutagenMetadataInputGroup(
+            "track_number",
+            metadata_display_panel=self.metadata_display_panel,
+            size_hint_y=None,
+            height=50,
+        )
+        self.recording_year_input_group = MutagenMetadataInputGroup(
+            "recording_year",
+            metadata_display_panel=self.metadata_display_panel,
+            size_hint_y=None,
+            height=50,
+        )
 
         self.metadata_panel.add_widget(self.artist_input_group)
         self.metadata_panel.add_widget(self.album_input_group)
--- metadata_setters.py
+++ metadata_setters.py
@@ -3,11 +3,8 @@ from typing import Iterable
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from mutagen import File
 from mutagen.id3 import TALB, TDRC, TIT2, TOPE, TPE1, TPE2, TRCK
-from mutagen.mp3 import HeaderNotFoundError
 from mutagen.oggopus import OggOpus
-from mutagen.wave import _WaveID3
 
 
 class BaseMetadataSetter:
@@ -53,15 +50,6 @@ class MP4MetadataSetter(BaseMetadataSetter):
         "track_number": "trkn",
         "recording_year": "\xa9day",
     }
-    # def __init__(self, filething, filepath, input_metadata_key, *args, **kwargs):
-    #     if input_metadata_key not in self.input_metadata_key_to_target_tag_map.keys():
-    #         raise ValueError("Input metadata key not supported. Supported input names: {}".format(
-    #             ",".join(self.input_metadata_key_to_target_tag_map.keys())
-    #         ))
-    #     super().__init__(*args, **kwargs)
-    #     self.filething = filething
-    #     self.filepath = filepath
-    #     self.input_metadata_key = input_metadata_key
 
     def set_tag(self, value):
         key = self.input_metadata_key_to_target_tag_map[self.input_metadata_key]
@@ -71,9 +59,6 @@ class MP4MetadataSetter(BaseMetadataSetter):
         if key == "trkn":
             value = [[int(value), 0]]
         self.filething.tags[key] = value
-
-    # def save_tags_to_file(self):
-    #     self.filething.save(self.filepath)
 
 
 class VorbisMetadataSetter(BaseMetadataSetter):
@@ -92,22 +77,6 @@ class VorbisMetadataSetter(BaseMetadataSetter):
         "track_number": "tracknumber",
         "recording_year": "date",
     }
-    # def __init__(self, filething, filepath, input_metadata_key, *args, **kwargs):
-    #     if input_metadata_key not in self.input_metadata_key_to_target_tag_map.keys():
-    #         raise ValueError("Input metadata key not supported. Supported input names: {}".format(
-    #             ",".join(self.input_metadata_key_to_target_tag_map.keys())
-    #         ))
-    #     super().__init__(*args, **kwargs)
-    #     self.filething = filething
-    #     self.filepath = filepath
-    #     self.input_metadata_key = input_metadata_key
-
-    # def set_tag(self, value):
-    #     key = self.input_metadata_key_to_target_tag_map[self.input_metadata_key]
-    #     self.filething.tags[key] = value
-
-    # def save_tags_to_file(self):
-    #     self.filething.save(self.filepath)
 
 
 class ID3MetadataSetter(BaseMetadataSetter):
@@ -125,16 +94,6 @@ class ID3MetadataSetter(BaseMetadataSetter):
         "recording_year": TDRC,
     }
 
-    # def __init__(self, tags, filepath, input_metadata_key, *args, **kwargs):
-    #     if input_metadata_key not in self.input_metadata_key_to_id3_tag_map.keys():
-    #         raise ValueError("Input metadata key not supported. Supported input names: {}".format(
-    #             ",".join(self.input_metadata_key_to_id3_tag_map.keys())
-    #         ))
-    #     super().__init__(*args, **kwargs)
-    #     self.tags = tags
-    #     self.filepath = filepath
-    #     self.input_metadata_key = input_metadata_key
-
     def set_tag(self, value):
         mapped_ID3_tags = self.input_metadata_key_to_id3_tag_map[
             self.input_metadata_key
@@ -144,7 +103,4 @@ class ID3MetadataSetter(BaseMetadataSetter):
             mapped_ID3_tags = [mapped_ID3_tags]
         for tag in mapped_ID3_tags:
             filled_tag = tag(encoding=3, text=value)
-            self.tags[filled_tag.__class__.__name__] = filled_tag
-
-    # def save_tags_to_file(self):
-    #     self.tags.save(self.filepath)
+            self.filething[filled_tag.__class__.__name__] = filled_tag
